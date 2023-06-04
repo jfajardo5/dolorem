3a60d4795a6142875176a2a2525d81e4ebdcfd50
Julio Fajardo <jfajardoh5@gmail.com>
2023-06-04 01:59:15 -0400

    Populated text property

--- dolorem.go
+++ dolorem.go
@@ -1,3 +1,8 @@
+// MIT License
+// Copyright (c) 2023 Julio "jfajardo5" Fajardo
+
+// See end of file for extended license information
+
 package dolorem
 
 import (
@@ -77,3 +82,25 @@ func (d *Dolorem) Paragraph(length ...int) string {
 func loadDictionary() []string {
 	return strings.Split(dictionary, "\n")
 }
+
+// Dolorem is a highly inclusive (with a near complete Latin dictionary) Lorem Ipsum generator
+//
+// Copyright (c) 2023 Julio "jfajardo5" Fajardo
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
