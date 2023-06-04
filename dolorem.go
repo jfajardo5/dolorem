8340356b898b2122305150ac7dc8d2974d525e67
Julio Fajardo <jfajardoh5@gmail.com>
2023-06-04 01:01:05 -0400

    Added edge case for sen_len

--- dolorem.go
+++ dolorem.go
@@ -37,31 +37,30 @@ func (d *Dolorem) Word() string {
 func (d *Dolorem) Sentence(length ...int) string {
 	sen_len := 15
 	if len(length) > 0 {
-		sen_len = length[0] // Number of words per Sentence
+		sen_len = length[0] // Number of Words per Sentence
 	}
 	sentence := ""
 	for i := 0; i < sen_len; i++ {
 		sentence = sentence + d.Word()
-		if i < sen_len-1 {
+		if i < sen_len-1 || i == 0 {
 			sentence = sentence + " "
 		}
-		continue
 	}
 	return sentence
 }
 
 func (d *Dolorem) Paragraph(length ...int) string {
-	num := 1
-	par_len := 7
-	sen_len := 15
+	num := 1      // Number of Paragraphs
+	par_len := 7  // Number of Sentences per Paragraph
+	sen_len := 15 // Number of Words per Sentence
 	if len(length) > 0 {
-		num = length[0] // Number of paragraphs
+		num = length[0] // Number of Paragraphs
 	}
 	if len(length) > 1 {
-		par_len = length[1] // Number of sentences per Paragraph
+		par_len = length[1] // Number of Sentences per Paragraph
 	}
 	if len(length) > 2 {
-		sen_len = length[2] // Number of words per Sentence
+		sen_len = length[2] // Number of Words per Sentence
 	}
 	var paragraph = d.paragraph_starter
 	for i := 0; i < num; i++ {
